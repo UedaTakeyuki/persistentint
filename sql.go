d5325910bbd2f1f5fb52814bd111f91a3a052e1c
Takeyuki UEDA <ueda@latelierdueda.com>
2022-10-29 16:30:40 +0900

    fix

--- sql.go
+++ sql.go
@@ -40,7 +40,7 @@ func (i *PersistentInt64) readDB() (value int64, err error) {
 
 	// make Query string
 	var querybuilder qb.Query
-	querybuilder.SetTableName(i.tname)
+	querybuilder.SetTableName("persistentints")
 	queryStr := querybuilder.Select([]string{"Value"}).Where(qb.Equal("ID", i.cname)).QueryString()
 
 	// make Query struct
@@ -58,8 +58,8 @@ func (i *PersistentInt64) saveDB() (err error) {
 
 	// make Query string
 	var querybuilder qb.Query
-	querybuilder.SetTableName(i.tname)
-	queryStr := querybuilder.ReplaceInto([]qb.Param{{Name: "Value", Value: i.Value}}).Where(qb.Equal("ID", i.cname)).QueryString()
+	querybuilder.SetTableName(persistentints)
+	queryStr := querybuilder.ReplaceInto([]qb.Param{{NAME: "ID", Value: i.cname}, {Name: "Value", Value: i.Value}}).Where(qb.Equal("ID", i.cname)).QueryString()
 
 	// make Query struct
 	query := dbhandle.Query{}
